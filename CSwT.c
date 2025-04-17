78ce8ae100278544261b1293703889fcbcc8049f
Kaibek <125792942+Kaibek@users.noreply.github.com>
2025-04-17 23:33:25 +0600

    minor corrections CSwT.c
    adding comments

--- CSwT.c
+++ CSwT.c
@@ -12,6 +12,7 @@ typedef struct
 
 
 
+
 // Решение трёхдиагональной системы методом прогонки или алгоритм Томаса
 static int tridiagonal_solve(int n, db a[], db b[], db c[], db d[], db k[])
 {
@@ -19,7 +20,8 @@ static int tridiagonal_solve(int n, db a[], db b[], db c[], db d[], db k[])
 
 	db a_prime[N];
 	db b_prime[N];
-
+	
+	// т.к y1 = b1, alpha1 = -c1/b1, beta1 = d1/y1
 	a_prime[0] = c[0] / b[0];
 	b_prime[0] = d[0] / b[0];
 
@@ -27,14 +29,15 @@ static int tridiagonal_solve(int n, db a[], db b[], db c[], db d[], db k[])
 	for (int i = 1; i < n - 1; i++)
 	{
 		db y_mid = b[i] - a[i] * a_prime[i - 1];
-		if (y_mid < 0) return -2;
+		if (y_mid < 0) return -1;
 
 		a_prime[i] = c[i] / y_mid;
 		b_prime[i] = (d[i] - a[i] * b_prime[i - 1]) / y_mid;
 	}
 
-	db y_mid = b[n - 1] - a[n - 1] * a_prime[n - 2];
-	if (y_mid < 0) return -3;
+	//конечные точки
+	db y_mid = b_prime[n - 2] - a[n - 1] * a_prime[n - 2];
+	if (y_mid < 0) return -1;
 
 	b_prime[n - 1] = (d[n - 1] - a[n - 1] * b_prime[n - 2]) / y_mid;
 
@@ -46,33 +49,32 @@ static int tridiagonal_solve(int n, db a[], db b[], db c[], db d[], db k[])
 	return 0;
 }
 
-
 // Построение кубического сплайна
 static int build_cubic_spline(int n, db x_vals[], db y[], Spline splines[])
 {
-	if (n <= 1 || n > N || !x_vals || !y) return -14;
+	if (n <= 1 || n > N || !x_vals || !y) return -1;
 
 	for (int i = 0; i < n - 1; i++)
-		if (x_vals[i] > x_vals[i + 1]) return -15;
+		if (x_vals[i] >= x_vals[i + 1]) return -1;
 
-	db  h[N - 1];								// Шаги
-	db	a[N] = { 0.0 }, b[N] = { 0.0 };					// Поддиагональ,  Диагональ,
-	db	c[N] = { 0.0 }, d[N] = { 0.0 };					// Наддиагональ, Правая часть
-	db  k[N];								// Вторые производные
+	db h[N - 1];							// Шаги
+	db a[N] = { 0.0 }, b[N] = { 0.0 };				// Поддиагональ,  Диагональ,
+	db c[N] = { 0.0 }, d[N] = { 0.0 };				// Наддиагональ, Правая часть
+	db k[N];							// Вторые производные
 
-	// Вычисление шагов
-	for (int i = 0; i < n - 1; i++)
-	{
+	// Вычисление шагов	
+	for (int i = 0; i < n - 1; i++) {
 		h[i] = x_vals[i + 1] - x_vals[i];
-		if (h[i] == 0.0) return -16;
+		if (h[i] == 0.0) return -1;
 	}
-	// Формируем трехдиагональную систему, т.к. нужно задать граничные узлы, чтобы система была полной
+
+	// Граничные условия
 	b[0] = 1.0;
 	d[0] = 0.0;
 	b[n - 1] = 1.0;
 	d[n - 1] = 0.0;
 
-	// Вычисляем для внутренних узлов
+	// Внутренние узлы
 	for (int i = 1; i < n - 1; i++)
 	{
 		a[i] = h[i - 1];
@@ -80,14 +82,13 @@ static int build_cubic_spline(int n, db x_vals[], db y[], Spline splines[])
 
 		b[i] = 2 * (a[i] + c[i]);
 		d[i] = 3 * ((y[i + 1] - y[i]) / c[i] - (y[i] - y[i - 1]) / a[i]);
-
 	}
 
 
 	int ret = tridiagonal_solve(n, a, b, c, d, k);
 	if (ret) return ret;
 
-	// Вычисляем коэффициенты сплайна
+	// Коэффициенты сплайна
 	for (int i = 0; i < n - 1; i++)
 	{
 		Spline* tmp = &splines[i];
@@ -113,7 +114,7 @@ static int build_cubic_spline(int n, db x_vals[], db y[], Spline splines[])
 
 static int binary_search(Spline splines[], double x, int n)
 {
-	if (n <= 0 || n > N || !splines) return -17;
+	if (n <= 0 || n > N || !splines) return -1;
 
 	int left = 0, right = n - 1;
 	while (left <= right)
@@ -137,15 +138,15 @@ static int binary_search(Spline splines[], double x, int n)
 // Интерполяция значения или вычисление значения сплайна в точке x
 static int interpolate(Spline splines[], double x, int n, double* result)
 {
-	if (n <= 0 || n > N || !splines || !result) return -18;
+	if (n <= 0 || n > N || !splines || !result) return -1;
 
-	if (x < splines[0].x)
-	{
+	if (x < splines[0].x) 
+{
 		*result = splines[0].a;
 		return 0;
 	}
 
-	if (x > splines[n - 1].x)
+	if (x > splines[n - 1].x) 
 	{
 		*result = splines[n - 1].a;
 		return 0;
@@ -163,20 +164,20 @@ static int interpolate(Spline splines[], double x, int n, double* result)
 }
 
 
-int main()
+int main() 
 {
 	db x_vals[N] = { 1, 2, 3, 4, 5 };
 	db y_vals[N] = { 120.0, 130.0, 110.0, 110.0, 120.0 };
 	Spline splines[N];
 
-	if (build_cubic_spline(N, x_vals, y_vals, splines) != 0)
+	if (build_cubic_spline(N, x_vals, y_vals, splines) != 0) 
 	{
 		printf("Error while constructing spline.\n");
-		return -19;
+		return -1;
 	}
 
-	for (double x = 1; x <= 5; x += 0.5)
-	{
+	for (double x = 1; x <= 5; x += 0.5) 
+	{ 
 		double result = 0;
 		if (interpolate(splines, x, N, &result) == 0)
 			printf("S(%g) = %g\n", x, result);
@@ -186,7 +187,6 @@ int main()
 
 	return 0;
 }
-
 ////////////////////////////////
 //Output:
 //S(1) = 120 => S(1.5) = 128.482
